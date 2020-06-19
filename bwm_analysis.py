d52eb586c342667567a703ca067e7b17c84b4bea
Paul T. Baker <paultbaker@gmail.com>
2020-06-19 07:16:03 -0500

    new runtime options

--- bwm_analysis.py
+++ bwm_analysis.py
@@ -11,7 +11,7 @@ except:
     import cPickle as pickle
 
 from utils import models
-from utils.sample_helpers import JumpProposal, get_parameter_groups
+from utils import sample_helpers as model_utils
 
 from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
@@ -20,15 +20,15 @@ from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 parser = argparse.ArgumentParser(
           description='run the BWM analysis with enterprise')
 
-parser.add_argument('-e', '--ephem',
-                    dest='ephem', default='DE436',
+parser.add_argument('-d', '--datafile',
+                    dest='datafile', default='~/nanograv/data/nano11.pkl',
                     action='store',
-                    help="JPL ephemeris version to use")
+                    help="pickle file containing array of enterprise Pulsar objects")
 
-parser.add_argument('-d', '--datadir',
-                    dest='datadir', default='~/nanograv/data/',
+parser.add_argument('-n', '--noisefile',
+                    dest='noisefile', default='~/nanograv/data/nano11_setpars.pkl',
                     action='store',
-                    help="location of data and noise pickles")
+                    help="pickle file containing noise parameters for all pulsars")
 
 parser.add_argument('-o', '--outdir',
                     dest='outdir', default='~/nanograv/bwm/',
@@ -45,26 +45,53 @@ parser.add_argument('--phi', type=float,
                     action='store',
                     help="sky position: phi")
 
+parser.add_argument('--tmin', type=float,
+                    dest='tmin', default=None,
+                    action='store',
+                    help="min search time (MJD)")
+
+parser.add_argument('--tmax', type=float,
+                    dest='tmax', default=None,
+                    action='store',
+                    help="max search time (MJD)")
+
 parser.add_argument('-u', '--upper-limit',
                     dest='UL', default=False,
                     action='store_true',
-                    help="use uniform priors suitable for upper limit\
-                          calculation. False for log-uniform priors for\
-                          detection")
+                    help=["use uniform priors suitable for upper limit ",
+                          "calculation. False for log-uniform priors for ",
+                          "detection"])
 
 parser.add_argument('-b', '--bayes-ephem',
                     dest='BE', default=False,
                     action='store_true',
                     help="use 'BayesEphem' ephemeris modeling")
 
+parser.add_argument('-g', '--dmgp',
+                    dest='DMGP', default=False,
+                    action='store_true',
+                    help=["use gaussian process DM variation modeling",
+                          "(instead of DMX"])
+
 parser.add_argument('-N', '--Nsamp', type=int,
                     dest='N', default=int(1.0e+06),
                     action='store',
                     help="number of samples to collect (before thinning)")
 
+parser.add_argument('--Nmax', type=int,
+                    dest='Nmax', default=int(1.0e+05),
+                    action='store',
+                    help="Maximum number of thinned samples when resuming")
+
+parser.add_argument('--write-hot',
+                    dest='write_hot', default=False,
+                    action='store_true',
+                    help="write hot PT chains")
+
+
 args = parser.parse_args()
 
-if args.costh and args.phi:
+if args.costh is not None and args.phi is not None:
     if args.costh > 1 or args.costh < -1:
         raise ValueError("costheta must be in range [-1, 1]")
     if args.phi > 2*np.pi or args.phi < 0:
@@ -79,6 +106,7 @@ else:
     err = "for fixed sky location must provide BOTH phi and costheta"
     raise RuntimeError(err)
 
+
 try:
     subprocess.run(['mkdir', '-p', args.outdir])
 except:
@@ -86,14 +114,13 @@ except:
     subprocess.call('mkdir -p ' + args.outdir, shell=True)
 
 # read in data pickles
-filename = args.datadir + 'nano11_{}.pkl'.format(args.ephem)
-with open(filename, "rb") as f:
+with open(args.datafile, "rb") as f:
     psrs = pickle.load(f)
 
-filename = args.datadir + 'nano11_setpars.pkl'
-with open(filename, "rb") as f:
+with open(args.noisefile, "rb") as f:
     setpars = pickle.load(f)
 
+print("loaded pickles")
 
 #################
 ##  pta model  ##
@@ -103,19 +130,37 @@ logmaxA = -11
 
 tmin = np.min([p.toas.min() for p in psrs]) / 86400
 tmax = np.max([p.toas.max() for p in psrs]) / 86400
-tclip = (tmax - tmin) * 0.05
-t0min = tmin + tclip
-t0max = tmax - tclip
+
+if args.tmin is not None and args.tmax is not None:
+    if args.tmin<tmin:
+        err = "tmin ({:.1f}) BEFORE first TOA ({:.1f})".format(args.tmin, tmin)
+        raise RuntimeError(err)
+    elif args.tmax>tmax:
+        err = "tmax ({:.1f}) AFTER last TOA ({:.1f})".format(args.tmax, tmax)
+        raise RuntimeError(err)
+    elif args.tmin>args.tmax:
+        err = "tmin ({:.1f}) BEFORE last tmax ({:.1f})".format(args.tmin, args.tmax)
+        raise RuntimeError(err)
+    else:
+        t0min = args.tmin
+        t0max = args.tmax
+else:
+    tclip = (tmax - tmin) * 0.05
+    t0min = tmin + tclip*2  # clip first 10%
+    t0max = tmax - tclip    # last 5%
+
 
 pta = models.model_bwm(psrs,
                        upper_limit=args.UL, bayesephem=args.BE,
+                       dmgp=args.DMGP,
                        logmin=logminA, logmax=logmaxA,
                        Tmin_bwm=t0min, Tmax_bwm=t0max,
                        skyloc=skyloc)
 pta.set_default_params(setpars)
 
+print("generated model")
 
-outfile = args.outdir + 'params.txt'
+outfile = args.outdir + '/params.txt'
 with open(outfile, 'w') as f:
     for pname in pta.param_names:
         f.write(pname+'\n')
@@ -132,19 +177,26 @@ ndim = len(x0)
 cov = np.diag(np.ones(ndim) * 0.1**2)
 
 # parameter groupings
-groups = get_parameter_groups(pta)
+groups = model_utils.get_parameter_groups(pta)
 
 sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior,
                  cov, groups=groups, outDir=args.outdir, resume=True)
 
 # add prior draws to proposal cycle
-jp = JumpProposal(pta)
-sampler.addProposalToCycle(jp.draw_from_prior, 15)
-sampler.addProposalToCycle(jp.draw_from_bwm_prior, 15)
-
-draw_bwm_loguni = jp.build_log_uni_draw('log10_A_bwm', logminA, logmaxA)
-sampler.addProposalToCycle(draw_bwm_loguni, 20)
+jp = model_utils.JumpProposal(pta)
+sampler.addProposalToCycle(jp.draw_from_prior, 5)
+sampler.addProposalToCycle(jp.draw_from_bwm_prior, 10)
+if args.BE:
+    sampler.addProposalToCycle(jp.draw_from_ephem_prior, 10)        
+if args.DMGP:
+    sampler.addProposalToCycle(jp.draw_from_dmgp_prior, 10)      
+    sampler.addProposalToCycle(jp.draw_from_dm1yr_prior, 10)      
+#if args.UL:
+#    draw_bwm_loguni = build_log_uni_draw('bwm_log10_A', logminA, logmaxA)
+#    sampler.addProposalToCycle(draw_bwm_loguni, 10)
 
 
 # SAMPLE!!
-sampler.sample(x0, args.N, SCAMweight=35, AMweight=10, DEweight=50)
+sampler.sample(x0, args.N,
+               SCAMweight=30, AMweight=20, DEweight=50,
+               writeHotChains=args.write_hot)
